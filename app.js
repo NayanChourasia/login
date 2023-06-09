bd7369e51d5da0f9b5c5ffa52a30be5773cc02b9
NayanChourasia <nayankr547@gmail.com>
2023-06-09 11:21:28 +0530

    commited with .env file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser =  require("body-parser");
@@ -25,8 +25,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "This is our little secret.";
-userSchema.plugin(encrypt, {secret: secret,   encryptedFields: ["password"]} );
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET,   encryptedFields: ["password"]} );
 
 const User = new mongoose.model("User", userSchema);
 
